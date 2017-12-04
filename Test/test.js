4fbd492baa21d8bb38f803b60d3c7edad9a5df77
dan <omnidbeerman@gmail.com>
2017-12-04 10:01:00 -0700

    File passes test

--- Test/test.js
+++ Test/test.js
@@ -4,7 +4,7 @@ var exercise = require("../app.js");
 describe("Warmup testing drill", ()=> {
   describe("#findFirstLetter", ()=> {
     it("returns the first character of a string", ()=> {
-      assert.equal(exercise.findFirstLetter("kevin"), "k"))
+      assert.equal(exercise.findFirstLetter("kevin"), "k");
     })
   })
 })
